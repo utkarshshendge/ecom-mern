bd136111a82e7d817602b5f0cca241ee6107ce42
bugsy07 <64540195+bugsy07@users.noreply.github.com>
2021-12-13 00:04:32 +0530

    Productslist.jsx file updated

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -5,6 +5,7 @@ import Products from "../components/Products";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
 import { mobile } from "../responsive";
+import { useLocation } from "react-router";
 
 const Container = styled.div``;
 
@@ -37,6 +38,8 @@ const Select = styled.select`
 const Option = styled.option``;
 
 const ProductList = () => {
+  const location = useLocation();
+  console.log(location.pathname.split("/")[2])
   return (
     <Container>
       <Navbar />
